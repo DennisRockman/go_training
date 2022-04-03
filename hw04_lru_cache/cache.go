f7ff5da6b3aeeacbfcf1bc754fd145cf7421afbc
Dennis Rockman <dneprovsky.dy@gmail.com>
2022-04-03 20:43:44 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -8,9 +8,41 @@ type Cache interface {
 	Clear()
 }
 
-type lruCache struct {
-	Cache // Remove me after realization.
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	item, isPresent := l.items[key]
+	cItem := &cacheItem{key: key, value: value}
+	if isPresent {
+		item.Value = cItem
+		l.queue.MoveToFront(item)
+	} else {
+		l.items[key] = l.queue.PushFront(cItem)
+		cItem, ok := l.queue.Back().Value.(*cacheItem)
+		if l.queue.Len() > l.capacity && ok {
+			delete(l.items, cItem.key)
+			l.queue.Remove(l.queue.Back())
+		}
+	}
+	return isPresent
+}
 
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	item, isPresent := l.items[key]
+	if isPresent {
+		cItem, ok := item.Value.(*cacheItem)
+		if ok {
+			l.queue.MoveToFront(item)
+			return cItem.value, true
+		}
+	}
+	return nil, false
+}
+
+func (l *lruCache) Clear() {
+	l.queue = NewList()
+	l.items = make(map[Key]*ListItem, l.capacity)
+}
+
+type lruCache struct {
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -10,6 +10,92 @@ type List interface {
 	MoveToFront(i *ListItem)
 }
 
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v}
+	if l.head == nil {
+		l.head = newItem
+		l.tail = newItem
+	} else {
+		l.insertBefore(newItem, l.head)
+	}
+	l.len++
+	return newItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v}
+	if l.tail == nil {
+		newItem = l.PushFront(v)
+	} else {
+		l.insertAfter(newItem, l.tail)
+		l.len++
+	}
+	return newItem
+}
+
+func (l *list) Remove(i *ListItem) {
+	l.lazyRemove(i)
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev != nil {
+		l.lazyRemove(i)
+		l.insertBefore(i, l.head)
+	}
+}
+
+func (l *list) insertAfter(newItem, i *ListItem) {
+	newItem.Prev = i
+	if i.Next == nil {
+		l.tail = newItem
+	} else {
+		newItem.Next = i.Next
+		i.Next.Prev = newItem
+	}
+	i.Next = newItem
+}
+
+func (l *list) insertBefore(newItem, i *ListItem) {
+	newItem.Next = i
+	if i.Prev == nil {
+		l.head = newItem
+	} else {
+		newItem.Prev = i.Prev
+		i.Prev.Next = newItem
+	}
+	i.Prev = newItem
+}
+
+func (l *list) lazyRemove(i *ListItem) {
+	if i.Prev == nil {
+		l.head = i.Next
+	} else {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next == nil {
+		l.tail = i.Prev
+	} else {
+		i.Next.Prev = i.Prev
+	}
+
+	i.Prev = nil
+	i.Next = nil
+}
+
 type ListItem struct {
 	Value interface{}
 	Next  *ListItem
@@ -17,8 +103,8 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head, tail *ListItem
+	len        int
 }
 
 func NewList() List {
